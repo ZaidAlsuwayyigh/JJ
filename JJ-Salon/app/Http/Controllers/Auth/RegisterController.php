9b1d6c095db8bfa93b0b5529de3004122ff142d1
Zaidomar0555 <zaidomar0555@gmail.com>
2021-01-26 14:40:00 +0300

    fixed user and role tables, i am able to have an admin who can have a full authority over users, whereas users will only view/manage their own things

--- JJ-Salon/app/Http/Controllers/Auth/RegisterController.php
+++ JJ-Salon/app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use App\Role;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -63,10 +64,15 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user =  User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        // the three lines below will make whoever is registering will be already a user.
+        $role = Role::select('id')->where('name', 'user')->first();
+        $user->roles()->attach($role);
+
+        return $user;
     }
 }
--- JJ-Salon/routes/web.php
+++ JJ-Salon/routes/web.php
@@ -17,3 +17,18 @@ Route::get('/', function () {
 
 // this is how they use routes in laravel 5.8.*
 // Route::get('/test', 'TestController@index')->name('test');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', function(){
+    return 'you are admin';
+})->middleware(['auth', 'auth.admin']);
+
+
+
+
+Route::namespace('Admin')->prefix('admin')->middleware(['auth', 'auth.admin'])->name('admin.')->group(function(){
+  Route::resource('/users', 'UserController', ['except' => ['show', 'create', 'store']] );
+});
